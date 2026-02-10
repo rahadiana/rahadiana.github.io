42f4588d5dfdfc5acb9524e52635d84b01f6f9dc
rahadiana nugraha <rahadiana@users.noreply.github.com>
2026-02-11 01:43:55 +0700

    Update main.js

--- okx/js/main.js
+++ okx/js/main.js
@@ -33,7 +33,7 @@ import { initAutoAttach } from './detectors/initHiddenLiquidity.js';
 // Configuration
 // const WS_URL = 'wss://eofficev2.bekasikota.go.id/okx-ws';
 // const WS_URL = 'ws://localhost:8020';
-const WS_URL = 'wss://myokx.nusantaracode.com';
+const WS_URL = 'wss://okx-ws.nusantaracode.com';
 
 const VIEWS = {
     'GLOBAL': ViewGlobal,
@@ -878,3 +878,4 @@ try {
 window.app = window.app || {};
 window.app.selectCoin = (coin) => selectCoin(coin, true);
 window.dashboardNavigate = (coin) => selectCoin(coin, true);
+
